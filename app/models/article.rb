eda9be13982f413f85d0d63a109c58ebefc811d2
“Decio” <deciocavalcantedeandrade@gmail.com>
2022-01-22 03:16:08 -0300

    Implementação dos testes de request /articles pendentes

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,6 @@ class Article < ApplicationRecord
     has_many :launches, dependent: :destroy
     has_many :events, dependent: :destroy
 
-    validates :featured, presence: true
     validates :title, presence: true
     validates :url, presence: true, uniqueness: { case_sensitive: true }
     validates :imageUrl, presence: true, uniqueness: { case_sensitive: true }
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it { is_expected.to validate_presence_of(:featured) }
+ 
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:url) }
   it { is_expected.to validate_presence_of(:imageUrl) }
