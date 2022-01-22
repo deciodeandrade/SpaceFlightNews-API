55a9b4af38438e00b579e05f05d7d7359e0d91c9
“Decio” <deciocavalcantedeandrade@gmail.com>
2022-01-22 10:18:59 -0300

    Refatorando controller + testes de model: Devido mudança nas associações

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  it { is_expected.to belong_to :article }
+  it { is_expected.to have_many(:article_events).dependent(:destroy) }
+  it { is_expected.to have_many(:articles).through(:article_events) }
 end
--- spec/models/launch_spec.rb
+++ spec/models/launch_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Launch, type: :model do
-  it { is_expected.to belong_to :article }
+  it { is_expected.to have_many(:article_launches).dependent(:destroy) }
+  it { is_expected.to have_many(:articles).through(:article_launches) }
 end
